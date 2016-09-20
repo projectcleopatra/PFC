75e88c70cfb007df289326edb01f2bdf41210ed2
Cheng Yu <passion.gratefulness.focus@gmail.com>
2016-09-20 22:57:16 +1000

    Some basic structure

---
+++ SameOrNot.py
@@ -0,0 +1,32 @@
+def openFilesAndMakeFeatures():
+    dataset = None
+
+
+    return dataset
+
+
+def trainModelAndEval(traindataSet, test):
+    results = []
+
+    return results
+
+
+def main():
+    '''
+    1. Open the training files and labels, make dataset
+    2. Go through each sample in the dataset, train the model, leave a few for validation
+    3. Use the model to predict
+    :return:
+    '''
+    train_dataset = openFilesAndMakeFeatures()
+    test_dataset = openFilesAndMakeFeatures()
+    results = trainModelAndEval(train_dataset, test_dataset)
+    
+
+
+
+    return
+
+
+if __name__ == "__main__":
+   main()
